330b0feea65d631ed79dd6b0995ddf571b387509
rama <alvimrama@gmail.com>
2017-11-27 14:41:44 -0200

    Backtrack finalizado Branch and bound (menos moedas) finalizado
    Branch and bound (mais variedade) Finalizado
    
    obs. O branch and bound (mais variedades) tem um tempo de exeucucao 1 cada decimal menor do que o branch and bound com mais moedas.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+
 
 
 public class Main {
@@ -11,58 +15,71 @@ public class Main {
         int[] tipomoedas = new int[]{10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1};
         int[] resposta = new int[tipomoedas.length];
         int[] valores = new int[tipomoedas.length];
-        int troco = 100;
+        int troco=0;
+        Scanner keyboard = new Scanner(System.in);
 
+        System.out.println("qual o valor de troco? ");
 
+        troco = keyboard.nextInt();
+
+        //le o arquivo txt com os valores
         valores=LerValores(tipomoedas,valores);
 
-     /*   for (int i = 0; i < valores.length; i++) {
-            System.out.println(valores[i]);
-        }
-      */
-        final long tempoInicialBacktrack = System.nanoTime();
+        System.out.println("Deseja utilizar o backtrack ou o branch and bound? ");
+        System.out.println("Entre com 1 para backtrack ou 2 para branch and bound ");
 
-        resposta= Backtrack.BackTrackInicio(tipomoedas,resposta,valores,troco);
+        int opcao=0;
 
-        final long tempofinalbacktrack=System.nanoTime() - tempoInicialBacktrack;
-        System.out.println("o metodo executou em " + (tempofinalbacktrack));
+        opcao=keyboard.nextInt();
 
-        for (int i = 0; i < resposta.length; i++) {
-            System.out.println(resposta[i]);
-        }
+        if(opcao==1)
+        {
+            final long tempoInicialBacktrack = System.nanoTime();
 
-        final long tempoInicialBranchBound = System.nanoTime();
+            resposta= Backtrack.BackTrackInicio(tipomoedas,resposta,valores,troco);
 
-        resposta= BranchBound.BBkinicio(tipomoedas,resposta,valores,troco);
+            final long tempofinalbacktrack=System.nanoTime() - tempoInicialBacktrack;
+            System.out.println("o metodo executou em " + (tempofinalbacktrack)+"ns");
 
-        final long tempofinalbb=System.nanoTime() - tempoInicialBranchBound;
-        System.out.println("o metodo executou em " + (tempofinalbb));
 
-        for (int i = 0; i < resposta.length; i++) {
-            System.out.println(resposta[i]);
+            for (int i = 0; i < resposta.length; i++) {
+                System.out.println(resposta[i]+ " de "+ tipomoedas[i] +" centavos"  );
+            }
         }
 
-        final long tempoInicialBranchBoundvariedades = System.nanoTime();
 
+        if(opcao==2)
+        {
+            final long tempoInicialBranchBound = System.nanoTime();
 
-        resposta=BranchBound.BBVariedadekinicio(tipomoedas,resposta,valores,troco);
+            resposta= BranchBound.BBkinicio(tipomoedas,resposta,valores,troco);
 
+            final long tempofinalbb=System.nanoTime() - tempoInicialBranchBound;
+            System.out.println("o metodo executou em " + (tempofinalbb)+"ns");
 
-        final long tempofinalbbvariedades=System.nanoTime() - tempoInicialBranchBoundvariedades;
-        System.out.println("o metodo executou em " + (tempofinalbbvariedades));
-
-
-        //System.out.println("A diferença do maior pro menor tempo de execucao é"+ ( Math.max(tempofinalbacktrack,tempofinalbb)  -  Math.min(tempofinalbacktrack,tempofinalbb) ) +"ns" );
-
-
-
-    //   /*
-
-        for (int i = 0; i < resposta.length; i++) {
-            System.out.println(resposta[i]);
+            for (int i = 0; i < resposta.length; i++) {
+                System.out.println(resposta[i]+ " de "+ tipomoedas[i] +" centavos" );
+            }
         }
 
-   //     */
+
+
+
+    //    final long tempoInicialBranchBoundvariedades = System.nanoTime();
+
+
+   //     resposta=BranchBound.BBVariedadekinicio(tipomoedas,resposta,valores,troco);
+
+
+     //   final long tempofinalbbvariedades=System.nanoTime() - tempoInicialBranchBoundvariedades;
+    //    System.out.println("o metodo executou em " + (tempofinalbbvariedades));
+
+
+
+  //      for (int i = 0; i < resposta.length; i++) {
+     //       System.out.println(resposta[i]+ " de "+ tipomoedas[i] +" centavos" );
+     //   }
+
 
     }//fim main
 
@@ -292,19 +309,36 @@ class BranchBound
 
     public static int[] BBVariedadekinicio(int[] tipomoedas, int[] resposta,int[] valoresfixo,int troco)
     {
+
+        tipomoedas=Reverte(tipomoedas);
+
+
+        //calcula o maximo de variedades possiveis
+        int lb=0;//valor maximo com a variedade maxima
+        int varlb=0;//maximo de variedade possivel
+        for(int i=0;i<tipomoedas.length;i++)
+        {
+            if(tipomoedas[i]<troco)
+            {
+                lb=lb+tipomoedas[i];
+                varlb=varlb+1;
+            }
+        }
+
+
         int[][] instancias=new int[tipomoedas.length][tipomoedas.length];
         int[] instancia;
 
-        for(int i=0;i<tipomoedas.length;i++)
+        for(int i=tipomoedas.length-varlb;i<tipomoedas.length;i++)
         {
             int[] valores=valoresfixo.clone();
             resposta=new int[tipomoedas.length];
-            instancias[i]=BBvariedade(tipomoedas,resposta,valores,i,troco,0,i);
+            instancias[i]=BBvariedade(tipomoedas,resposta,valores,i,troco,0,0);
         }
         instancia=instancias[0];
         for(int i=1;i<tipomoedas.length;i++)
         {
-            instancia =RetornaMaior(instancia,instancias[i]);
+            instancia =RetornaMaiorvariedade(instancia,instancias[i]);
         }
         return instancia;
     }
@@ -358,12 +392,12 @@ class BranchBound
         if(qualmoeda==tipomoedas.length-1 && valor<troco && valores[qualmoeda]>0)
         {//ultima pos
 
-            if(ub>=valor) {
+
                 valores[qualmoeda] = valores[qualmoeda] - 1;
                 resposta[qualmoeda] = resposta[qualmoeda] + 1;
 
                 resposta = BB(tipomoedas, resposta, valores, qualmoeda, troco, valor + tipomoedas[qualmoeda], ini);
-            }
+
 
         }
 
@@ -372,67 +406,23 @@ class BranchBound
 
     }
 
-    private static int[] BBvariedade(int[] tipomoedas, int[] resposta,int[] valores, int qualmoeda,int troco,int valor,int ini)
+    private static int[] BBvariedade(int[] tipomoedas, int[] resposta,int[] valores, int qualmoeda,int troco,int valor,int var)
     {
 
-        int lb=troco -(valor+tipomoedas[qualmoeda]);//lower bound
 
-        if(valor==troco && valores[qualmoeda]>=0)
-        {//ok
-
-            //achou troco
-            return resposta;
-
-        }
-        if(valor<troco && qualmoeda==tipomoedas.length-1 && valores[tipomoedas.length-1]==0)
-        {//sem troco
-            //sem moeda daquele valor pra acrescentar ao troco
-            //sem mais moedas para dar
-
-            resposta=new int[resposta.length];//vetor com apenas 0
-            return resposta;
-        }
-        if(qualmoeda<tipomoedas.length-1&& valor<troco && valores[qualmoeda]>0) {
-            //em valor acrescenta-se uma moeda do tipo tipomoedas[qualmoeda] na resposta
-            //e se deixa aberta a possibilidade de colocar outra moeda igual
-            //testa se existe a cedula ou moeda na quantia
-            //se tiver tudo ok, testa o upper bound
-
-            if(troco>lb) {
-
-                valores[qualmoeda] = valores[qualmoeda] - 1;
-                resposta[qualmoeda] = resposta[qualmoeda] + 1;
-                resposta = BBvariedade(tipomoedas, resposta, valores, qualmoeda, troco, valor + tipomoedas[qualmoeda], ini);
-            }
+        if(tipomoedas[qualmoeda]+valor<=troco && valores[qualmoeda]>0)
+        {
+            resposta[qualmoeda]=resposta[qualmoeda]+1;
+            valores[qualmoeda]=valores[qualmoeda]-1;
+            resposta=BBvariedade(tipomoedas,resposta,valores,qualmoeda+1,troco,valor+tipomoedas[qualmoeda],var);
 
         }
-        if(qualmoeda<tipomoedas.length-1&& valor<troco && valores[qualmoeda]==0) {
-            //sem moeda daquele valor
-            //nao é nem preciso testar o upper bound, ja que nao tem jeito de colocar moedas nesse caso
-            resposta=BBvariedade(tipomoedas,resposta,valores,qualmoeda+1,troco,valor,ini);
-
-        }
-        if(qualmoeda<tipomoedas.length-1 && valor>troco && qualmoeda>=ini && valores[qualmoeda]>0)
-        {//em valor, retira-se uma moeda do tipo tipomoedas[qualmoeda] da resposta
-            //e exclui-se a possibilidade de caber outra moeda do mesmo tipo.
-            //e coloca-se a moeda que foi retirada de volta no caixa
-            valores[qualmoeda]=valores[qualmoeda]+1;
+        if(tipomoedas[qualmoeda]+valor>troco && valores[qualmoeda]>0)
+        {
             resposta[qualmoeda]=resposta[qualmoeda]-1;
-            resposta=BBvariedade(tipomoedas,resposta,valores,qualmoeda+1,troco,valor-tipomoedas[qualmoeda],ini);
+            valores[qualmoeda]=valores[qualmoeda]+1;
+            resposta=BBvariedade(tipomoedas,resposta,valores,qualmoeda-1,troco,valor-tipomoedas[qualmoeda],var);
         }
-
-        if(qualmoeda==tipomoedas.length-1 && valor<troco && valores[qualmoeda]>0)
-        {//ultima pos
-
-
-                valores[qualmoeda] = valores[qualmoeda] - 1;
-                resposta[qualmoeda] = resposta[qualmoeda] + 1;
-
-                resposta = BBvariedade(tipomoedas, resposta, valores, qualmoeda, troco, valor + tipomoedas[qualmoeda], ini);
-
-
-        }
-
         return resposta;
 
 
@@ -458,14 +448,18 @@ class BranchBound
     }
 
 
-    public static int[] RetornaMaior(int[] inst1,int[] inst2)
+    public static int[] RetornaMaiorvariedade(int[] inst1,int[] inst2)
     {
         int qtd1=0,qtd2=0;
 
         for(int i=0;i<inst1.length;i++)
         {
-            qtd1=qtd1+inst1[i];
-            qtd2=qtd2+inst2[i];
+            if(inst1[i]>0) {
+                qtd1++;
+            }
+            if(inst2[i]>0) {
+                qtd2++;
+            }
         }
         if(qtd1>qtd2)
         {
@@ -477,6 +471,13 @@ class BranchBound
         }
     }
 
-
+    public static int[] Reverte(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        return array;
+    }
 
 }//fim branch bound
